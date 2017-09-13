598c629ab734054f2b749be6e4d3e9c66d34bc5b
jm <heartfeats@gmail.com>
2017-09-13 13:25:35 -0700

    add html, css, js business logic, js user interface logic

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+
+// business logic:
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// user interface logic:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+
+    $("#output").show();
+
+    $("#output").text(result);
+  })
+});
